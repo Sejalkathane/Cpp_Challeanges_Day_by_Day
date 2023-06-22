1038d09d75abc8d9115f1eec986ef399b2ef2e9b
Sejal kathane <kathanesejal@gmail.com>
2023-06-23 05:19:22 +0530

    Day 5:

--- Day5/415_Add_Strings.cpp
+++ Day5/415_Add_Strings.cpp
@@ -3,6 +3,40 @@ using namespace std;
 
 int main()
 {
+    string a="113345767";
+    string b="1237888";
+
+    int i=a.length()-1;
+    int j=b.length()-1;
     
+    string ans="";
+    int carry=0;
+
+    while(i>=0 || j>=0 ||carry)
+    {
+        int sum=0;
+        if(i>=0)
+        {
+            sum+=(a[i]-'0');
+            i--;
+        }
+
+        if(j>=0)
+        {
+            sum+=(b[j]-'0');
+            j--;
+        }
+
+        sum+=carry;
+        carry=sum/10;
+        sum=sum%10;
+        ans=ans+ to_string(sum);
+    }
+    reverse(ans.begin(),ans.end());
+
+   cout<<ans;
+
+
+
     return 0;
 }
---
+++ Day6/989_Add_to_Array_Form_of_Integer.cpp
@@ -0,0 +1,18 @@
+// The array-form of an integer num is an array representing its digits in left to right order.
+// For example, for num = 1321, the array form is [1,3,2,1].
+// Given num, the array-form of an integer, and an integer k, return the array-form of the integer num + k.
+
+// Example 1:
+// Input: num = [1,2,0,0], k = 34
+// Output: [1,2,3,4]
+// Explanation: 1200 + 34 = 1234
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}

4a5aec0f087aad8278db6161c9bf66d67ccd8588
Sejal kathane <kathanesejal@gmail.com>
2023-06-26 23:56:55 +0530

    Day 9

---
+++ Day9 KD,SW/1456_Maximum_Number_ofVowels_in_a_Substring_of_Given_Length.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+// Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+
+// Example 1:
+
+// Input: s = "abciiidef", k = 3
+// Output: 3
+// Explanation: The substring "iii" contains 3 vowel letters.
+
+int main()
+{
+    string s="abciiidef";
+    int k=3;
+}
